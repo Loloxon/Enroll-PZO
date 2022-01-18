1f5b4966cfeb3cea4d9e9ecb7a759ea54566470f
loloxon <nikodemko@interia.pl>
2022-01-18 16:05:04 +0100

    i guess im gettin better it this :P

--- relax.py
+++ relax.py
@@ -3,9 +3,11 @@ from random import random, randint
 S = [1 for i in range(174)]
 G = [1 for i in range(127)]
 
-for i in range(20):
-    rS = randint(0,174-1)
-    rG = randint(0,127-1)
+for i in range(35):
+    rS = randint(0, 174 - 1)
+    while S[rS] == 0:
+        rS = randint(0, 174 - 1)
+    rG = randint(0, 127 - 1)
     S[rS] = 0
     # G[rG] = 0
 print("relaxS = ", S, ";", sep="")
