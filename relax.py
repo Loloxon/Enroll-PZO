2f033c353c6cc0a5a82f5355e268f53170463775
loloxon <nikodemko@interia.pl>
2022-01-18 14:09:31 +0100

    improve got cleared from shit

--- relax.py
+++ relax.py
@@ -3,9 +3,9 @@ from random import random, randint
 S = [1 for i in range(174)]
 G = [1 for i in range(127)]
 
-for i in range(10):
-    rS = randint(0,174-1);
-    rG = randint(0,127-1);
+for i in range(20):
+    rS = randint(0,174-1)
+    rG = randint(0,127-1)
     S[rS] = 0
     # G[rG] = 0
 print("relaxS = ", S, ";", sep="")
