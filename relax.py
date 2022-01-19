d7e79f743cf6c7ef9addd1b108200005eff22deb
loloxon <nikodemko@interia.pl>
2022-01-19 16:36:35 +0100

    gettin better res

--- relax.py
+++ relax.py
@@ -4,13 +4,13 @@ S = [1 for _ in range(174)]
 class_size = [15, 174, 15, 174, 15, 30, 174, 15, 174, 15, 174, 15, 174, 15, 174, 15, 174, 30, 174, 15, 174, 15, 174, 15, 174];
 C = [1 for _ in range(25)]
 
-for i in range(10):
+for i in range(40):
     rS = randint(0, 174 - 1)
     while S[rS] == 0:
         rS = randint(0, 174 - 1)
-    # S[rS] = 0
-
-for i in range(7):
+    S[rS] = 0
+# 12 to wykłady - 13 to zajęcia
+for i in range(10):
     rC = randint(0, 25 - 1)
     while C[rC] == 0 or class_size[rC] == 174:
         rC = randint(0, 25 - 1)
