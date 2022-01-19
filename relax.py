d6280eeef9bfc58f0e09e0f447f6c598ea0d9cec
loloxon <nikodemko@interia.pl>
2022-01-19 02:47:54 +0100

    wtf, got better objective then Matt

--- relax.py
+++ relax.py
@@ -1,19 +1,24 @@
 from random import randint
 
 S = [1 for _ in range(174)]
+class_size = [15, 174, 15, 174, 15, 30, 174, 15, 174, 15, 174, 15, 174, 15, 174, 15, 174, 30, 174, 15, 174, 15, 174, 15, 174];
 C = [1 for _ in range(25)]
 
-for i in range(15):
+for i in range(10):
     rS = randint(0, 174 - 1)
     while S[rS] == 0:
         rS = randint(0, 174 - 1)
-    S[rS] = 0
+    # S[rS] = 0
 
-for i in range(3):
+for i in range(4):
     rC = randint(0, 25 - 1)
-    while S[rC] == 0:
+    while C[rC] == 0 or class_size[rC] == 174:
         rC = randint(0, 25 - 1)
     C[rC] = 0
 
 print("relaxS = ", S, ";", sep="")
 print("relaxC = ", C, ";", sep="")
+print()
+# for i in range(25):
+#     if class_size[i] != 174:
+#         print(C[i], class_size[i])
