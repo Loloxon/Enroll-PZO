c737f3d00f5fffe7a9a9b1a313cff7c8fcd3e42e
loloxon <nikodemko@interia.pl>
2022-01-19 01:23:05 +0100

    lets trick competition 8)

--- relax.py
+++ relax.py
@@ -1,14 +1,19 @@
-from random import random, randint
+from random import randint
 
-S = [1 for i in range(174)]
-G = [1 for i in range(127)]
+S = [1 for _ in range(174)]
+C = [1 for _ in range(25)]
 
-for i in range(35):
+for i in range(15):
     rS = randint(0, 174 - 1)
     while S[rS] == 0:
         rS = randint(0, 174 - 1)
-    rG = randint(0, 127 - 1)
     S[rS] = 0
-    # G[rG] = 0
+
+for i in range(3):
+    rC = randint(0, 25 - 1)
+    while S[rC] == 0:
+        rC = randint(0, 25 - 1)
+    C[rC] = 0
+
 print("relaxS = ", S, ";", sep="")
-print("relaxG = ", G, ";", sep="")
+print("relaxC = ", C, ";", sep="")
